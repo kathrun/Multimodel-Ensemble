12470a75bfdb74f652a6d035e550d0b3c6610859
Kathryn <82959861+kathrun@users.noreply.github.com>
2023-08-18 09:27:16 -0500

    a few graphing scripts

--- Graphing_Scripts/quadplot.py
+++ Graphing_Scripts/quadplot.py
@@ -3,109 +3,100 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.ticker as ticker
 from cycler import cycler
+from spacepy.plot import style 
 
-#set style information
-#create own style?
-plt.style.use('seaborn-notebook')
+style()
+
+# set style information
 mpl.rcParams['lines.linewidth'] = 3
 mpl.rcParams["figure.titlesize"]  = 20
 mpl.rcParams['axes.labelsize'] = 16
 mpl.rcParams['xtick.labelsize'] = 12
 mpl.rcParams['ytick.labelsize'] = 12
-plt.rcParams['figure.constrained_layout.use'] = True
 
-#data for graphs
-#how to pull automatically from analyze/output files?
+# data for graphs
 npc = [1, 2, 3, 4, 5]
 
-#legend
-#ax1.legend(loc='upper left')
-#plt.legend(frameon=False)
-
-#creating subplots
-#not sure this is the most effecent way to create them but it works?
+# creating subplots
 fig = plt.figure()
-gs = fig.add_gridspec(2,2)
-(ax1, ax2), (ax3, ax4) = gs.subplots(sharex=True, sharey=True)
-fig.subplots_adjust(hspace=0.15, wspace=0)
+gs = fig.add_gridspec(2, 2)
+(ax1, ax2), (ax3, ax4) = gs.subplots(sharex=True, sharey=False)
+fig.set_tight_layout(True)
+fig.subplots_adjust(hspace=0.15, wspace=0.1)
+fig.suptitle('0.3nT/s Threshold')
 
 
-#0.3 Threshold
-#HSS Data
-H_all = 0.032, 0.007, -0.151, -0.308, -0.489
-H_hi = 0.074, 0.007, -0.119, -0.239, -0.386
-H_lo = -0.040, 0.003, -0.190, -0.391, -0.545
+# PoD
+all = 0.151, 0.051, 0.009, 0, 0
+hi = 0.161, 0.058, 0.013, 0.001, 0.001
+low = 0.125, 0.034, 0.001, -0.001, -0.001
 
-#set labels and titles
-ax1.set_title('0.3nT/s Threshold', fontsize = 16)
-ax1.set_ylabel('$\Delta$HSS', fontsize=14)
+# set labels and titles
+ax1.set_title('$\Delta$PoD', fontsize=16)
 
+# 0 line for refernce
+# ax1.axhline(y=1, color='grey', linestyle="--")
 
-#0 line for refernce
-ax1.axhline(y=0, color='grey', linestyle = "--")
+# plot mag groups
+ax1.plot(npc, all, '-oC1')
+ax1.plot(npc, hi, '-oC0')
+ax1.plot(npc, low, '-oC2')
 
-#plot mag groups
-ax1.plot(npc, H_all, '-oC1')
-ax1.plot(npc,H_hi, '-oC0')
-ax1.plot(npc, H_lo, '-oC2')
+# PoFD
+all = 0.112, 0.02, 0.004, 0.004, 0.004
+hi = 0.152, 0.032, 0.01, 0.008, 0.008
+low = 0.095, 0.014, 0.002, 0.002, 0.002
 
-#0.7 Threshold
-#HSS Data
-H_all = 0.026, -0.087, -0.289, -0.443, -0.525
-H_hi = 0.044, -0.015, -0.191, -0.327, -0.406
-H_lo = -0.052, -0.250, -0.483, -0.647, -0.697
+# set labels and titles
+ax2.set_title('-$\Delta$PoFD', fontsize=16)
 
-#set labels and titles
-ax2.set_title('0.7nT/s Threshold', fontsize = 16)
+# 0 line for refernce
+# ax2.axhline(y=0, color='grey', linestyle="--")
 
-#0 line for refernce
-ax2.axhline(y=0, color='grey', linestyle = "--")
+# plot mag groups
+ax2.plot(npc, all, '-oC1', label='All')
+ax2.plot(npc, hi, '-oC0', label='High')
+ax2.plot(npc, low, '-oC2', label="Low")
 
-#plot mag groups
-ax2.plot(npc, H_all, '-oC1', label='All')
-ax2.plot(npc,H_hi, '-oC0', label ='High')
-ax2.plot(npc, H_lo, '-oC2', label="Low")
-
-#1.1 Threshold
-#HSS Data
-H_all = 0.013,-0.161, -0.314, -0.407, -0.464
-H_hi = -0.004, -0.114, -0.246, -0.334, -0.394
-H_lo = 0.018, -0.294, -0.461, -0.536, -0.557
+# HSS
+all = 0.032, 0.03, 0.005, -0.003, -0.003 
+hi =  0.074, 0.044, 0.008, -0.003, -0.003 
+low =  -0.04, 0.005, -0.002, -0.004, -0.004
 
 #set labels and titles
-ax3.set_title('1.1nT/s Threshold', fontsize = 16)
-ax3.set_ylabel('$\Delta$HSS', fontsize=14)
-ax3.set_xlabel('NPC Members Required', fontsize=14)
-ax3.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax3.set_title('$\Delta$HSS', fontsize=16)
+ax3.set_xlabel('NPC', fontsize=14)
+ax3.set_xticks(npc)
+# ax3.xaxis.set_major_locator(ticker.MultipleLocator(1))
 
-#0 line for refernce
-ax3.axhline(y=0, color='grey', linestyle = "--")
+# 0 line for refernce
+# ax3.axhline(y=1, color='grey', linestyle="--")
 
-#plot mag groups
-ax3.plot(npc, H_all, '-oC1')
-ax3.plot(npc,H_hi, '-oC0')
-ax3.plot(npc, H_lo, '-oC2')
+# plot mag groups
+ax3.plot(npc, all, '-oC1')
+ax3.plot(npc, hi, '-oC0')
+ax3.plot(npc, low, '-oC2')
 
-#1.5 Threshold
-#HSS Data
-H_all = -0.006, -0.123, -0.270, -0.311, -0.385
-H_hi = -0.044, -0.113, -0.226, -0.275, -0.353
-H_lo = 0.047, -0.169, -0.383, -0.377, -0.411
+# Bias
+all = 0.181, 0.073, 0.015, 0.005, 0.005
+hi = -0.075, 0.074, 0.019, 0.006, 0.006
+low =  -0.024, 0.063, 0.007, 0.005, 0.005
 
-#set labels and titles
-ax4.set_title('1.5nT/s Threshold', fontsize = 16)
-ax4.set_xlabel('NPC Members Required', fontsize=14)
-ax4.xaxis.set_major_locator(ticker.MultipleLocator(1))
+# set labels and titles
+ax4.set_title('Change in Bias from 1', fontsize=16)
+ax4.set_xlabel('NPC', fontsize=14)
+# ax4.xaxis.set_major_locator(ticker.MultipleLocator(1))
 
-#0 line for refernce
-ax4.axhline(y=0, color='grey', linestyle = "--")
+# 0 line for refernce
+# ax4.axhline(y=1, color='grey', linestyle="--")
 
-#plot mag groups
-ax4.plot(npc, H_all, '-oC1')
-ax4.plot(npc,H_hi, '-oC0')
-ax4.plot(npc, H_lo, '-oC2')
+# plot mag groups
+ax4.plot(npc, all, '-oC1', label='All')
+ax4.plot(npc, hi, '-oC0', label='High')
+ax4.plot(npc, low, '-oC2', label="Low")
 
-#legend
-ax2.legend(loc='upper right', edgecolor='white', framealpha=1, title='Magnetometers')
+# legend
+ax4.legend(loc='upper right', edgecolor='white', framealpha=1,
+           title='Magnetometers')
 
 plt.show()
--- Graphing_Scripts/tri_v2_b.py
+++ Graphing_Scripts/tri_v2_b.py
@@ -2,95 +2,111 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.ticker as ticker
-import matplotlib.colors as mcolors
 from cycler import cycler
+from spacepy.plot import style 
 
-#set style information
-#create own style?
-plt.style.use('seaborn-notebook')
-mpl.rcParams['lines.linewidth'] = 2.5
-mpl.rcParams["figure.titlesize"]  = 14
+style()
+
+# set style information
+mpl.rcParams['lines.linewidth'] = 3
+mpl.rcParams["figure.titlesize"]  = 20
+mpl.rcParams['axes.labelsize'] = 16
 mpl.rcParams['xtick.labelsize'] = 12
 mpl.rcParams['ytick.labelsize'] = 12
 
-#data for graphs
-#how to pull automatically from analyze/output files?
+# data for graphs
 npc = [1, 2, 3, 4, 5]
 
-#0.3 Threshold
-#PoD 0.3
-D1_all = 0.151, 0.051, 0.009, 0.0, 0.0
-D1_hi = 0.161, 0.058, 0.013, 0.001, 0.001
-D1_lo = 0.125, 0.034, 0.001, -0.001, -0.001
+# 0.3 Threshold
+# PoD
+D1_all = 0.151, -0.028, -0.241, -0.418, -0.597
+D1_hi = 0.162, -0.013, -0.205, -0.373, -0.578
+D1_lo = 0.125, -0.064, -0.328, -0.523, -0.644
 
-#PoFD 0.3
-F2_all = -0.112, -0.020, -0.004, -0.004, -0.004
-F2_hi = -0.152, -0.032, -0.010, -0.008, -0.008
-F2_lo = -0.095, -0.014, -0.002, -0.002, -0.002
+D4_all = 0.145, -0.146, -0.371, -0.502, -0.566
+D4_hi = 0.176, -0.072, -0.299, -0.435, -0.578
+D4_lo = 0.056, -0.36, -0.581, -0.695, -0.644
 
-#HSS 0.3
-H3_all = 0.032, 0.030, 0.005, -0.003, -0.003
-H3_hi = 0.074, 0.044, 0.008, -0.003, -0.003
-H3_lo = -0.040, 0.005, -0.002, -0.004, -0.004
+D7_all = 0.112, -0.212, -0.374, -0.45, -0.491
+D7_hi = 0.123, -0.158, -0.328, -0.412, -0.462
+D7_lo = 0.081, -0.373, -0.513, -0.562, -0.576
 
-#0.7 Threshold
-#PoD 0.7
-D4_all = 0.145, 0.026, 0.001, 0.0, 0.0
-D4_hi = 0.176, 0.035, 0.002, 0.0, 0.0
-D4_lo = 0.056, -0.001, -0.001, -0.001, -0.001
+D10_all = 0.093, -0.176, -0.322, -0.356, -0.405
+D10_hi = 0.088, -0.146, -0.289, -0.333, -0.39
+D10_lo = 0.111, -0.272, -0.432, -0.432, -0.453
 
-#PoFD 0.7
-F5_all = -0.066, 0.005, -0.002, -0.002, -0.002
-F5_hi = -0.117, -0.009, -0.003, -0.003, -0.003
-F5_lo = -0.035, -0.002, -0.001, -0.001, -0.001
 
-#HSS 0.7
-H6_all = 0.026, 0.015, -0.002, -0.003, -0.003
-H6_hi = 0.044, 0.026, -0.001, -0.003, -0.003
-H6_lo = -0.052, -0.008, -0.003, -0.003, -0.003
+# -PoFD
+F2_all = -0.112, 0.033, 0.085, 0.105, 0.109
+F2_hi = -0.152, 0.028, 0.12, 0.158, 0.165
+F2_lo =  -0.095, 0.036, 0.071, 0.084, 0.086
 
-#1.1 Threshold
-#PoD 1.1
-D7_all = 0.112, 0.015, -0.001, -0.001, -0.001
-D7_hi = 0.123, 0.019, -0.002, -0.002, -0.002
-D7_lo = 0.081, 0.004, 0.004, 0.004, 0.004
+F5_all = -0.066, 0.031, 0.057, 0.061, 0.064
+F5_hi = -0.177, 0.049, 0.1, 0.109, 0.112
+F5_lo = -0.034, 0.021, 0.031, 0.033, 0.035
+ 
+F8_all = -0.044, 0.026, 0.05, 0.057, 0.059
+F8_hi = -0.09, 0.033, 0.076, 0.089, 0.092
+F8_lo = 0.013, 0.021, 0.03, 0.033, 0.034
 
-#PoFD 1.1
-F8_all = -0.045, -0.004, -0.001, -0.001, -0.001
-F8_hi = -0.089, -0.007, -0.003, -0.003, -0.003
-F8_lo = -0.012, -0.002, 0.0, 0.0, -0.004
+F11_all = -0.042, 0.026, 0.047, 0.051, 0.054
+F11_hi = -0.083, 0.026, 0.063, 0.07, 0.073
+F11_lo = -0.01, 0.028, 0.035, 0.038, 0.04
 
-#HSS 1.1
-H9_all = 0.013, 0.005, -0.003, -0.003, 0.003
-H9_hi = -0.004, 0.008, -0.006, -0.006, 0.006
-H9_lo = 0.018, -0.005, 0.003, -0.003, 0.003
+# HSS
+H3_all = 0.032, 0.008, -0.15, -0.308, -0.489
+H3_hi = 0.075, 0.008, -0.119, -0.239, -0.386
+H3_lo =  -0.04, 0.002, -0.191, -0.391, -0.546
 
-#1.5 Threshold
-#PoD 1.5
-D10_all = 0.093, 0.015, 0.003, 0.003, 0.003
-D10_hi = 0.088, 0.012, 0.002, 0.002, 0.002
-D10_lo = 0.111, 0.026, 0.001, 0.005, 0.005
+H6_all = 0.026, -0.087, -0.289, -0.443, -0.525
+H6_hi = 0.044, -0.016, -0.192, -0.327, -0.406
+H6_lo =  -0.052, -0.25, -0.484, -0.647, -0.697
 
-#PoFD 1.5
-F11_all = -0.042, -0.004, 0.0, 0.0, 0.0
-F11_hi = -0.083, -0.007, -0.001, -0.001, -0.001
-F11_lo = -0.010, -0.001, 0.0, 0.0, 0.0
+H9_all = 0.013, -0.161, -0.314, -0.407, -0.464
+H9_hi = -0.005, -0.115, -0.247, -0.334, -0.395
+H9_lo =  0.018, -0.294, -0.461, -0.536, -0.557
 
-#HSS 1.5
-H12_all = 0.013, 0.005, -0.003, -0.003, 0.003
-H12_hi = -0.004, 0.008, -0.006, -0.006, 0.006
-H12_lo = 0.018, -0.005, 0.003, -0.003, 0.003
+H12_all = -0.006, -0.123, -0.269, -0.311, -0.384
+H12_hi = -0.044, -0.114, -0.226, -0.276, -0.353
+H12_lo =  0.047, -0.169, -0.383, -0.377, -0.411
+
+# Bias 0.3
+B13_all = 0.09, -0.064, -0.334, -0.532, -0.716
+B13_hi = 0.23, -0.024, -0.258, -0.444, -0.652
+B13_lo = -0.238, -0.157, -0.512, -0.74, -0.866
+
+# 0.7
+B14_all = 0.161, -0.221, -0.509, -0.651, -0.721
+B14_hi = 0.27, -0.132, -0.423, -0.571, -0.65
+B14_lo = -0.154, -0.469, -0.757, -0.881, -0.929
+
+# 1.1
+B15_all = 0.215, -0.311, -0.565, -0.667, -0.716
+B15_hi =  0.287, -0.227, -0.491, -0.604, -0.661
+B15_lo = -0.001, -0.56, -0.784, -0.854, -0.882
+
+# 1.5
+B16_all =  0.202, -0.325, -0.589, -0.65, -0.713
+B16_hi = 0.344, -0.226, -0.492, -0.558, -0.625
+B16_lo = -0.255, -0.638, -0.893, -0.936, -0.989
 
 
 #creating subplots
-fig, ((ax1, ax2, ax3),(ax4, ax5, ax6),(ax7, ax8, ax9), (ax10, ax11, ax12)) = plt.subplots(4,3, figsize = (4, 3), sharey=True, sharex=True)
+fig, ((ax1, ax2, ax3, ax13),
+      (ax4, ax5, ax6, ax14),
+      (ax7, ax8, ax9, ax15),
+      (ax10, ax11, ax12, ax16)) = plt.subplots(4, 4, figsize=(4, 4),
+                                               sharey=True, sharex=True)
 
 fig.subplots_adjust(hspace=0, wspace=0)
+fig.supxlabel('NPC Members Required', fontsize=16)
+fig.set_tight_layout(True)
 #titles and lables!
 
 ax1.xaxis.set_major_locator(ticker.MultipleLocator(1))
 ax2.xaxis.set_major_locator(ticker.MultipleLocator(1))
 ax3.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax13.xaxis.set_major_locator(ticker.MultipleLocator(1))
 
 #0.3 Threshold plot
 #plot PoD
@@ -111,7 +127,7 @@ ax2.plot(npc, F2_all, '-oC0', label='All')
 ax2.plot(npc, F2_hi, '-oC1', label='High')
 ax2.plot(npc, F2_lo, '-oC2', label='Low')
 
-ax2.axhline(y=0, color='grey', linestyle = "--")
+ax2.axhline(y=0, color='grey', linestyle="--")
 #ax2.fill_between(npc, y1=-0.7, y2=0, color ='gainsboro')
 
 #plot HSS
@@ -195,18 +211,47 @@ ax11.plot(npc, F11_lo, '-oC2', label='Low')
 ax11.axhline(y=0, color='grey', linestyle = "--")
 #ax11.fill_between(npc, y1=-0.7, y2=0, color ='gainsboro')
 
-ax11.set_xlabel('NPC Members Required', fontsize=16)
-
 #plot HSS
 ax12.plot(npc, H12_all, '-oC0', label='All')
-ax12.plot(npc,H12_hi, '-oC1', label ='High')
+ax12.plot(npc, H12_hi, '-oC1', label ='High')
 ax12.plot(npc, H12_lo, '-oC2', label="Low")
 
 ax12.axhline(y=0, color='grey', linestyle = "--")
 #ax12.fill_between(npc, y1=0, y2=0.2, color ='gainsboro')
 
+# Bias
+ax13.set_title('Change in Bias from 1',fontsize=16)
+# plot Bias 0.3
+ax13.plot(npc, B13_all, '-oC0', label='All')
+ax13.plot(npc, B13_hi, '-oC1', label ='High')
+ax13.plot(npc, B13_lo, '-oC2', label="Low")
+
+ax13.axhline(y=0, color='grey', linestyle="--")
+
+# plot Bias 0.7
+ax14.plot(npc, B14_all, '-oC0', label='All')
+ax14.plot(npc, B14_hi, '-oC1', label ='High')
+ax14.plot(npc, B14_lo, '-oC2', label="Low")
+
+ax14.axhline(y=0, color='grey', linestyle="--")
+
+# plot Bias 1.1
+ax15.plot(npc, B15_all, '-oC0', label='All')
+ax15.plot(npc, B15_hi, '-oC1', label ='High')
+ax15.plot(npc, B15_lo, '-oC2', label="Low")
+
+ax15.axhline(y=0, color='grey', linestyle="--")
+
+# plot Bias 1.5
+ax16.plot(npc, B16_all, '-oC0', label='All')
+ax16.plot(npc, B16_hi, '-oC1', label ='High')
+ax16.plot(npc, B16_lo, '-oC2', label="Low")
+
+ax16.axhline(y=0, color='grey', linestyle="--")
+
 #legend
-ax3.legend(loc='upper right', edgecolor='white', framealpha=1, title='Magnetometers')
+ax13.legend(loc='upper right', edgecolor='white', framealpha=1,
+            title='Magnetometers')
 
 
 plt.show()
